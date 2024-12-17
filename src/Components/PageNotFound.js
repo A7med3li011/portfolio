705136a72da45bf53da57610e4d0d68855c4bc02
a7med3li0111 <a7med3li0111@gmail.com>
2024-12-17 19:29:10 +0200

    last commit

--- src/Components/PageNotFound.js
+++ src/Components/PageNotFound.js
@@ -1,9 +1,13 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 
 export default function PageNotFound() {
+  const navigate = useNavigate()
   return (
-    <div>
-      not found
+    <div className='h-[100vh] bg-black text-white text-center'>
+      <p className='text-4xl mt-10 mb-4'>OOPS!</p>
+      <p className='mb-4'>Not Found Page!</p>
+      <button onClick={()=>navigate(-1)} className='bg-white text-black py-3 px-5 rounded-xl'>go back</button>
     </div>
   )
 }
